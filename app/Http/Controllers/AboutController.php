974ff8cfccb84008a7d35a7c8f3ad057913e4de0
tyonesatria <iostudio92@gmail.com>
2021-03-03 23:30:44 +0700

    first commit

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    public function about()
+    {
+        return "Nama :Muhammad Tyone Noor Satria <br> NIM : 2041723006";
+    }
+}
